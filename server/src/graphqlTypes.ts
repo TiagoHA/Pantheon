0b08b555db9be1d9518e22d1b3b813c2db44b8b7
LFSCamargo <luizepauloxd@gmail.com>
2018-12-04 16:27:05 -0200

    ⚡️ Adds server boilerplate code

---
+++ server/src/graphqlTypes.ts
@@ -0,0 +1,23 @@
+import { PubSub, gql } from 'apollo-server';
+import userTypes from './modules/user/userType';
+import { User } from './modules/user/userModel';
+
+export interface GraphQLContext {
+  user: User,
+  pubsub: PubSub,
+}
+
+const graphqlTypes = gql`
+  type Mutation {
+    register(name: String, email: String, password: String): AuthenticationOutput
+    login(email: String, password: String): AuthenticationOutput
+  }
+  
+  type Query {
+    me: User
+    user(_id: String): User
+    users(search: String, first: Int, after: Int): UserConnectionOutput
+  }
+`;
+
+export default [graphqlTypes, userTypes];
