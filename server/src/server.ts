abb461148c9142765909492bf1008ffd017b73e1
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-12-05 02:59:56 -0200

    change https to http in src/server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,39 @@
-import { ApolloServer, PubSub } from 'apollo-server';
-import * as mongoose from 'mongoose';
-import { MONGO, PORT } from './config';
-import graphqlTypes from './graphqlTypes';
-import resolvers from './resolvers';
-import { getUser } from './auth/authMethods';
+import { ApolloServer, PubSub } from 'apollo-server'
+import * as mongoose from 'mongoose'
+import { MONGO, PORT } from './config'
+import graphqlTypes from './graphqlTypes'
+import resolvers from './resolvers'
+import { getUser } from './auth/authMethods'
 
 const pubsub = new PubSub()
 
 const server = new ApolloServer({
-	resolvers,
-	typeDefs: graphqlTypes,
-	context: async ({ req }) => {
-		const token = req.headers.authorization ? req.headers.authorization : '';
-		const user = await getUser(token);
-		return {
-			user,
-			pubsub,
-		};
-	},
-	tracing: true,
-});
-
-mongoose.connect(MONGO, {}, err => {
-	if (err) {
-		console.log('Error: ', err);
-		process.exit(1);
-	}
-	console.log(`Connected to mongodb at: ${MONGO}`);
-
-	server.listen(PORT, () => {
-		console.log(`🚀 Apollo server ready on https://localhost:${PORT}/graphql`);
-		console.log(`⚡️ Playground exposed on /graphql`);
-	});
+  resolvers,
+  typeDefs: graphqlTypes,
+  context: async ({ req }) => {
+    const token = req.headers.authorization ? req.headers.authorization : ''
+    const user = await getUser(token)
+    return {
+      user,
+      pubsub,
+    }
+  },
+  tracing: true,
 })
+
+mongoose.connect(
+  MONGO,
+  {},
+  err => {
+    if (err) {
+      console.log('Error: ', err)
+      process.exit(1)
+    }
+    console.log(`Connected to mongodb at: ${MONGO}`)
+
+    server.listen(PORT, () => {
+      console.log(`🚀 Apollo server ready on http://localhost:${PORT}/graphql`)
+      console.log(`⚡️ Playground exposed on /graphql`)
+    })
+  },
+)
